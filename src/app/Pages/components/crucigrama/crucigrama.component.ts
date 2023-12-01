fc28144002aa4bb17dcdf1e241891faf3470d044
Ricardo Perez <rperezrodriguez@deloitte.es>
2023-12-01 13:07:16 +0100

    prueba

--- src/app/Pages/components/crucigrama/crucigrama.component.ts
+++ src/app/Pages/components/crucigrama/crucigrama.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { PagesRoutingModule } from '../../pages-routing.module';
 
 @Component({
   selector: 'app-inicio',
+  standalone: true,
+  imports: [MatButtonModule, PagesRoutingModule],
   templateUrl: './crucigrama.component.html',
   styleUrls: ['./crucigrama.component.scss'],
 })
-export class CrucigramaComponent {}
+export class CrucigramaComponent {
+  constructor(private router: Router) {}
+
+  volver() {
+    this.router.navigate(['/prueba1']);
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   // {
   //   path: 'auth',
   //   loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
@@ -20,9 +19,3 @@ const routes: Routes = [
     redirectTo: '404',
   },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
