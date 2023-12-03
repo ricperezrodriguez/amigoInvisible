8e01f1b923d3cec7b99552bbd7a582ec34df4477
Ricardo Perez <rperezrodriguez@deloitte.es>
2023-12-04 00:46:35 +0100

    fotos

--- src/app/Pages/components/asociar/asociar.component.ts
+++ src/app/Pages/components/asociar/asociar.component.ts
@@ -1,12 +1,27 @@
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-asociar',
   standalone: true,
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './asociar.component.html',
-  styleUrl: './asociar.component.scss'
+  styleUrl: './asociar.component.scss',
 })
 export class AsociarComponent {
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker',
+  ];
 
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 }
